8e54644d7e151cf6dcb923db57f95aa589413065
Dmitry Rakov <rakovdk@gmail.com>
2023-04-06 17:18:58 +0200

    The second commit of the HelloWorld project.

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -3,6 +3,6 @@ package com.example.helloworld;             //package statement
 public class HelloWorld {                   //class declaration
     //I use this comment modification just for test
     public static void main(String[] args) {
-        System.out.println("Hello World ver 0.1!");
+        System.out.println("Hello World ver 0.2!");
     }
 }
