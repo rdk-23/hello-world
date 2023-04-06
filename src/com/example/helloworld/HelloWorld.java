4bf9bac19049dc9ba1f20fc6475b4c7e3b88f945
Dmitry Rakov <rakovdk@gmail.com>
2023-04-06 17:13:23 +0200

    The first commit of the HelloWorld project.

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;             //package statement
+
+public class HelloWorld {                   //class declaration
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
